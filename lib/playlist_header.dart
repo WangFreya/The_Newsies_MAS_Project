abbfd39ff3aa12247bc4bedf69ae9d1499755322
Alyssa Behrend <agbehrend99@gmail.com>
2021-12-08 14:36:46 -0500

    adds state and makes more dynamic

--- lib/playlist_header.dart
+++ lib/playlist_header.dart
@@ -1,11 +1,37 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:newsies_sprint3_prototype/track.dart';
 
-class PlaylistHeader extends StatelessWidget {
+class PlaylistHeader extends StatefulWidget {
   const PlaylistHeader({Key? key}) : super(key: key);
 
   @override
+  _PlaylistHeaderState createState() => _PlaylistHeaderState();
+}
+
+class _PlaylistHeaderState extends State<PlaylistHeader> {
+  final track1 = Track(title: "Audio File 1", time: "5:06");
+  final track2 = Track(title: "Audio File 2", time: "4:57");
+  final track3 = Track(title: "Audio File 3", time: "5:12");
+  late List<Track> _tracks;
+  late String numFiles;
+
+  @override
+  void initState() {
+    super.initState();
+    _tracks = <Track>[track1, track2, track3];
+  }
+
+  // String calcRuntime() {
+  //   int runtime;
+  //   _tracks.forEach((Track track) {
+  //     min = track.time[0];
+  //   })
+  // }
+
+  @override
   Widget build(BuildContext context) {
+    numFiles = _tracks.length.toString();
     return Row(
       children: [
         Expanded(
@@ -20,13 +46,19 @@ class PlaylistHeader extends StatelessWidget {
               children: [
                 const SizedBox(width: 20),
                 TextButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      // Navigator.push(
+                      //   context,
+                      //   MaterialPageRoute(
+                      //       builder: (context) =>
+                      //           ListenerScreen(track: _tracks[0])));
+                    },
                     child: const Text('PLAY'),
                     style: TextButton.styleFrom(
                         shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(20.0)))),
                 SizedBox(width: 200.0),
-                const Text("3 files | 15 min")
+                Text(numFiles + " files | 15 min")
               ],
             )
           ],
