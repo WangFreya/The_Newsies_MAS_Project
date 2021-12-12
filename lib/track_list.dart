eccbbe575e2fd22eb848142b22a08eb96bf61991
Alyssa Behrend <agbehrend99@gmail.com>
2021-12-12 15:45:24 -0500

    fixes intializer bug

--- lib/track_list.dart
+++ lib/track_list.dart
@@ -11,14 +11,22 @@ class TrackList extends StatefulWidget {
 }
 
 class _TrackListState extends State<TrackList> {
-  List<Track>? _tracks;
+  // final track1 = Track(
+  //     title: "Part 1",
+  //     time: "5:03",
+  //     url:
+  //         "https://mas-newsies-output.s3.us-east-2.amazonaws.com/gatsby_snip.txt.mp3");
+  // final track2 = Track(title: "Part 2", time: "4:57", url: "");
+  // final track3 = Track(title: "Part 3", time: "5:12", url: "");
+  final List<Track> _tracks = [];
 
   @override
   void initState() {
     super.initState();
+
     for (var i = 0; i < 12; i++) {
-      _tracks?.add(Track(
-          title: "Part " + i.toString(),
+      _tracks.add(Track(
+          title: "Part " + (i + 1).toString(),
           time: "5:00",
           url:
               "https://mas-newsies-output.s3.amazonaws.com/05TheGreatGatsby_segment_" +
@@ -35,29 +43,30 @@ class _TrackListState extends State<TrackList> {
   }
 
   Widget _buildTrackList() {
-    return ListView.builder(
-      itemCount: _tracks?.length,
+    return Expanded(
+        child: ListView.builder(
+      itemCount: _tracks.length,
       padding: const EdgeInsets.all(16.0),
       scrollDirection: Axis.vertical,
       shrinkWrap: true,
       itemBuilder: (context, index) {
         return _buildTrack(index);
       },
-    );
+    ));
   }
 
   Widget _buildTrack(int index) {
     return ListTile(
-        title: Text(_tracks!.elementAt(index).title),
-        trailing: Text(_tracks!.elementAt(index).time),
+        title: Text(_tracks.elementAt(index).title),
+        trailing: Text(_tracks.elementAt(index).time),
         onTap: () {
           setState(() {
-            selectedTrack ??= _tracks!.elementAt(index);
+            selectedTrack ??= _tracks.elementAt(index);
 
             Navigator.push(
               context,
               MaterialPageRoute(
-                  builder: (context) => PlayPodcast(track: _tracks![index])),
+                  builder: (context) => PlayPodcast(track: _tracks[index])),
             );
           });
         });
