6d127e2374fb4ba516807143a6af9ac9f2fb92b4
jsanniota3 <jsanniota@gatech.edu>
2021-12-12 16:40:37 -0500

    change upload button to route to ChooseSnippetLength

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,9 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
+import 'package:newsies_sprint3_prototype/choose_snip_length.dart';
 import 'bookrow.dart';
+import 'choose_snip_length.dart';
 
 void main() => runApp(const MyApp());
 
@@ -12,18 +14,57 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Newsies Sprint 3',
-      home: Scaffold(
+    return MaterialApp(title: 'Newsies Sprint 3', home: MyHome());
+  }
+}
+// Scaffold(
+//   appBar: AppBar(
+//     title: const Text('Snippet Learning Prototype'),
+//   ),
+//   body: Center(
+//       child: Column(children: <Widget>[
+//     const Expanded(child: BookRow()),
+// TextButton(
+//     onPressed: () {
+//       Navigator.push(
+//         context,
+//         MaterialPageRoute(
+//             builder: (context) => const ChooseSnipLength()),
+//       );
+//     },
+//     child: const Text('Upload File'))
+//   Builder(
+// builder: (context) => Center(
+//       child: TextButton(
+//         child: Text("UploadFile"),
+//         onPressed: () => Navigator.pushNamed(context, ChooseSnipLength()),
+//       ),
+//     ),
+// ])),
+//     ;),
+//   );
+// }
+// }
+
+class MyHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
         appBar: AppBar(
           title: const Text('Snippet Learning Prototype'),
         ),
         body: Center(
             child: Column(children: <Widget>[
           const Expanded(child: BookRow()),
-          TextButton(onPressed: () {}, child: const Text('Upload File'))
-        ])),
-      ),
-    );
+          TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const ChooseSnipLength()),
+                );
+              },
+              child: const Text('Upload File'))
+        ])));
   }
 }
