67cf0df54260445fbfd07f712709838550f153dc
Freya Wang <wangfreya582@gmail.com>
2021-12-12 18:26:42 -0500

    Update reading_mat.dart

--- lib/reading_mat.dart
+++ lib/reading_mat.dart
@@ -1,10 +1,22 @@
 class ReadingMat {
   String title;
   String textFile;
+  int snippetLength;
 
-  ReadingMat({required this.title, required this.textFile});
+  ReadingMat(
+      {required this.title,
+      required this.textFile,
+      required this.snippetLength});
 
   getTitle() {
     return title;
+  }
+
+  getTextFile() {
+    return textFile;
+  }
+
+  getSnippetLength() {
+    return snippetLength;
   }
 }
