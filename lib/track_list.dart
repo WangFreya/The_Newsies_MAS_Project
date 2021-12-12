6a28766003a430d3a0dd2ecb622bc8599a81be2d
Alyssa Behrend <agbehrend99@gmail.com>
2021-12-12 17:41:57 -0500

    attempts time

--- lib/track_list.dart
+++ lib/track_list.dart
@@ -1,38 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
 import 'package:newsies_sprint3_prototype/bookrow.dart';
 import 'package:newsies_sprint3_prototype/play_podcast.dart';
 import 'package:newsies_sprint3_prototype/track.dart';
+import 'package:http/http.dart' as http;
+import 'package:mp3_info/mp3_info.dart';
 
 class TrackList extends StatefulWidget {
-  const TrackList({Key? key}) : super(key: key);
+  const TrackList({Key? key})
+      : super(
+          key: key,
+        );
 
   @override
   _TrackListState createState() => _TrackListState();
 }
 
 class _TrackListState extends State<TrackList> {
-  // final track1 = Track(
-  //     title: "Part 1",
-  //     time: "5:03",
-  //     url:
-  //         "https://mas-newsies-output.s3.us-east-2.amazonaws.com/gatsby_snip.txt.mp3");
-  // final track2 = Track(title: "Part 2", time: "4:57", url: "");
-  // final track3 = Track(title: "Part 3", time: "5:12", url: "");
   final List<Track> _tracks = [];
+  late String tempTime = '';
 
   @override
   void initState() {
-    super.initState();
-
     for (var i = 0; i < 12; i++) {
-      _tracks.add(Track(
-          title: "Part " + (i + 1).toString(),
-          time: "5:00",
-          url:
-              "https://mas-newsies-output.s3.amazonaws.com/05TheGreatGatsby_segment_" +
-                  i.toString() +
-                  ".txt.mp3"));
+      // int time = 0;
+      String url =
+          "https://mas-newsies-output.s3.amazonaws.com/05TheGreatGatsby_segment_" +
+              i.toString() +
+              ".txt.mp3";
+      //String time = "0";
+      // getTime(url).then((value) {
+      //   time = value!;
+      // });
+      getTime(url);
+      _tracks.add(
+          Track(title: "Part " + (i + 1).toString(), time: tempTime, url: url));
     }
+    super.initState();
+  }
+
+  void getTime(String url) async {
+    AudioPlayer player = AudioPlayer();
+    var duration = await player.setUrl(url);
+    setState(() {
+      tempTime = duration!.inSeconds.toString();
+    });
   }
 
   Track? selectedTrack;
